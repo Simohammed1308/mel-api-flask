6d71ef7a40fb9b07b1bd132fdc06793e4736489b
Simohammed1308 <elmoutbis@yahoo.fr>
2023-07-24 20:03:42 +0200

    Update ELMOUATASSIM_Mohammed_API_flask.py

--- ELMOUATASSIM_Mohammed_API_flask.py
+++ ELMOUATASSIM_Mohammed_API_flask.py
@@ -82,12 +82,12 @@ def credit(id_client):
     #scaler.fit(X_preprocess)
     #X_norm = scaler.transform(X_preprocess)
     
-    proba = load_clf.predict_proba(X)
+    #proba = load_clf.predict_proba(X)
     prediction = load_clf.predict(X)
     
     pred_proba = {
-        'prediction': int(prediction),
-        'proba': float(proba[0][0])
+        'prediction': int(prediction)
+        #,'proba': float(proba[0][0])
         
     }
 
